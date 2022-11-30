2aec87c1d5f382338fad95b1aff5efdcb538c0d0
Lucas Santana <lucas_377@hotmail.com>
2022-11-29 21:55:24 -0300

    Refactoring components

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import styled from 'styled-components';
 
 import Titulo from "../Titulo";
 import Conta from "../Conta";
 
+const ContainerWrapper = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`;
+
+const Content = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
 const Container = () => {
   return (
-    <div className="container">
+    <ContainerWrapper>
       <Titulo>Olá Fulano!</Titulo>
-      <section className="conteudo">
+      <Content>
         <Conta />
-      </section>
-    </div>
+      </Content>
+    </ContainerWrapper>
   );
 };
 
