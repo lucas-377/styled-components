a28d1b74dae7911b361eb4dcaed52136233916a9
Lucas Santana <lucas_377@hotmail.com>
2022-11-29 21:43:33 -0300

    Variant component via props

--- src/Components/Cabecalho/index.jsx
+++ src/Components/Cabecalho/index.jsx
@@ -17,17 +17,29 @@ const Logo = styled.img`
   width: 50px;
 `;
 
+const BtnHeader = styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+
+  color: ${(props) => props.primary ? corPrimaria : "white"};
+  background: ${(props) => props.primary ? "white" : corPrimaria};
+`;
+
 const Cabecalho = () => {
   return (
     <StyledHeader>
       <Logo src={bank_logo} alt="Logo Smart Bank" />
       <div>
-        <a className="btn-secundario" href="https://google.com">
+        <BtnHeader primary href="https://google.com">
           Ajuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
+        </BtnHeader>
+        <BtnHeader href="https://google.com">
           Sair
-        </a>
+        </BtnHeader>
       </div>
     </StyledHeader>
   );
